33471d49da5372814979a4997fdc4a761bbf4c4f
KukuhAgung <130808713+KukuhAgung@users.noreply.github.com>
2024-02-28 13:39:27 +0700

    alert fix

--- js/signin.js
+++ js/signin.js
@@ -3,33 +3,17 @@
 document.addEventListener("DOMContentLoaded", () => {
   const userForm = document.getElementById("userForm");
   const userManager = new User();
-  const Toast = Swal.mixin({
-    toast: true,
-    position: "top-end",
-    showConfirmButton: false,
-    timer: 1000,
-    timerProgressBar: true,
-    didOpen: (toast) => {
-      toast.addEventListener("mouseenter", Swal.stopTimer);
-      toast.addEventListener("mouseleave", Swal.resumeTimer);
-    },
-  });
 
   userForm.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    const usernameInput = document.getElementById("username").value;
+    const usernameByInput = document.getElementById("username").value;
 
-    const result = userManager.signInUser(usernameInput);
+    const result = userManager.signInUser(usernameByInput);
 
     if (result.success) {
-      localStorage.setItem("userLoggedIn", usernameInput);
-      Toast.fire({
-        icon: "success",
-        title: "Signed in successfully",
-      }).then(() => {
-        window.location.href = "../pages/tasks.html";
-      });
+      localStorage.setItem("userLoggedIn", usernameByInput);
+      window.location.href = "../pages/tasks.html";
     } else {
       console.log(result.message);
     }
